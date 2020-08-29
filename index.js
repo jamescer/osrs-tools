b115579a7a57f58d643fa5235c21f97ba674e0fc
BS <imzessed@gmail.com>
2020-08-29 16:43:51 -0400

    Fixed module exports 🛠

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var q = require('./questData.json');
 
-module.exports.json = function () {
+exports.json = function () {
 
     return q;
 }
-module.exports.hello = function () {
+exports.hello = function () {
     console.log("if you need help use the .help() method");
     console.log("This is a message from the demo package");
 }
-module.exports = (str) => {
-    return `💩${str}💩`;
-}
-module.exports.help = function () {
-    console.log("import q from \"osrs-quest-tool\";\n console.log(q.json);");
 
+// exports = (str) => {
+//     return `💩${str}💩`;
+// }
+exports.help = function () {
+    console.log("import q from \"osrs-quest-tool\";\n console.log(q.json());");
 }

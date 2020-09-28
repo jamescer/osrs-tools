b6aa056b29ffb23d00c810167b4ac7e379f0837a
BS <imzessed@gmail.com>
2020-09-27 20:11:17 -0400

    Vampire Slayer => Vampyre Slayer

--- manage.py
+++ manage.py
@@ -15,7 +15,22 @@ def makeJsonToArray():
     with open('testarray.json', 'w') as outfile:
         json.dump(d2, outfile)
 
-makeJsonToArray()
+
+data = []
+d2 = []
+with open('questData.json') as json_file:
+    data = json.load(json_file)
+
+for key in data:
+    val = data[key]
+    if val['requirements']:
+
+        if len(val['requirements']['quests']) >= 1:
+            print('quests', str(key))
+        elif len(val['requirements']['quests']) == 0:
+            print('no quests', str(key))
+        else:
+            print('ERROR', key)
 # We will need to make a solid array of tasks per diary?
 # so instead of ArdougneDiary.easy.tasks its
 # arrayEasy = [

5e564b186bed0cb89ebbe6605996f0f0b8a30d8a
James Cerniglia <jamesmcerniglia@gmail.com>
2025-08-19 01:41:30 -0400

    Publish tsconfig changes

--- src/model/quest/all/DesertTreasure.ts
+++ src/model/quest/all/DesertTreasure.ts
@@ -1,23 +1,5 @@
 import { Skill } from '../../account/Skill';
-import { QuestDifficulty, QuestLength } from '../enums';
-export interface QuestRewards {
-  experience: Array<{ skill: Skill; amount: number }>;
-  questPoints: number;
-  items: any[];
-  areas: any[];
-  unlocks: any[];
-  lamps: any[];
-  points: any[];
-  pets: any[];
-}
+import { QuestDifficulty, QuestLength } from '../enums';  
 
-const rewards: QuestRewards = {
-  experience: [{ skill: Skill.Magic, amount: 20000 }],
-  questPoints: 3,
-  items: [],
-  areas: [],
-  unlocks: [],
-  lamps: [],
-  points: [],
-  pets: [],
-};
+import { Quest } from '../types';
+import { QuestStatus } from '../Quest';

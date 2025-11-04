b993c1b902bafadd053aa6033fe7f22bf2645bb9
James Cerniglia <jamesmcerniglia@gmail.com>
2025-11-04 16:30:41 -0500

    update cc

--- jest.config.js
+++ jest.config.js
@@ -2,16 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  coverageReporters: ['text', 'lcov', 'clover', 'cobertura', 'json-summary'],
   coverageDirectory: 'coverage',
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80
-    }
-  },
-  testMatch: ['**/test/**/*.test.ts'],
-  verbose: true
+  coverageReporters: ['lcov', 'text']
 };
